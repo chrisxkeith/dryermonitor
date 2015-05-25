bac6b965c6e2a7a7d765cb27ca36ad4d9fa75050
chrisxkeith <chris.keith@gmail.com>
2015-05-25 15:10:44 -0700

    Remove unused code.

--- readCurrentSensor/readCurrentSensor.ino
+++ readCurrentSensor/readCurrentSensor.ino
@@ -1,38 +1,33 @@
-int sensorPin = A0;	// select the input pin for the potentiometer
-int ledPin = 13;  	// select the pin for the LED
-//int sensorValue = 0;  // variable to store the value coming from the sensor
+const int sensorPin = A0;	// select the input pin for the electrical current sensor.
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);  
   Serial.begin(9600);
-  Serial.println("ampl  peakVolt  rms     amps");
+  Serial.println("ampl\tpeakVlt\tRMS\tamps");
 }
 
 void loop() {
-  int maxVal = 0;
   int minVal = 1024;
-  long startTime = micros();
-  const int iters = 148;
+  int maxVal = 0;
+  const int iters = 150;
   for (int i = 0; i < iters; i++) {
-	int sensorValue = analogRead(sensorPin); 
-	maxVal  = max(sensorValue, maxVal);
-	minVal  = min(sensorValue, minVal);
+    int sensorValue = analogRead(sensorPin); 
+    minVal  = min(sensorValue, minVal);
+    maxVal  = max(sensorValue, maxVal);
+    delay(1);
   }
-  long endTime = micros();
-  long duration = (endTime - startTime) / iters;
   int amplitude = maxVal - minVal;
   float peakVoltage = (5.0 / 1024) * amplitude / 2;
   float rms = peakVoltage / 1.41421356237;
-  float amps = rms * 30; // TODO: what ois 30?
+  float amps = rms * 30; // TODO: what is 30?
   Serial.print(amplitude);
-  Serial.print("    ");
+  Serial.print("\t");
   Serial.print(peakVoltage);
-  Serial.print("      ");
+  Serial.print("\t");
   Serial.print(rms);
-  Serial.print("    ");
-  Serial.println(amps);
- 
-  delay(2000);          
+  Serial.print("\t");
+  Serial.print(amps); 
+  Serial.println("");
+  
+  delay(5000);          
 }
 
