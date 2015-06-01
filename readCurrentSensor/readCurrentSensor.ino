618791ad48827030e51b906fa044de393afb634f
chrisxkeith <chris.keith@gmail.com>
2015-05-31 18:44:39 -0700

    start refactoring for sending status to Twitter

--- readCurrentSensor/readCurrentSensor.ino
+++ readCurrentSensor/readCurrentSensor.ino
@@ -1,8 +1,34 @@
+#include <stdio.h>
+
 const int sensorPin = A0;	// select the input pin for the electrical current sensor.
+char* message;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("ampl\tpeakVlt\tRMS\tamps");
+  if ((message = (char *)malloc(255)) == NULL) {
+    Serial.println("message malloc failed.");
+  }
+}
+
+void toTwitter(char* message) {
+}
+
+void toSerial(char* message) {
+  Serial.println("here");
+}
+
+char *ftoa(char *a, double f, int precision){
+   long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+   
+   char *ret = a;
+   long heiltal = (long)f;
+   itoa(heiltal, a, 10);
+   while (*a != '\0') a++;
+   *a++ = '.';
+   long desimal = abs((long)((f - heiltal) * p[precision]));
+   itoa(desimal, a, 10);
+   return ret;
 }
 
 void loop() {
@@ -16,18 +42,23 @@ void loop() {
     delay(1);
   }
   int amplitude = maxVal - minVal;
-  float peakVoltage = (5.0 / 1024) * amplitude / 2;
-  float rms = peakVoltage / 1.41421356237;
-  float amps = rms * 30; // TODO: what is 30?
-  Serial.print(amplitude);
-  Serial.print("\t");
-  Serial.print(peakVoltage);
-  Serial.print("\t");
-  Serial.print(rms);
-  Serial.print("\t");
-  Serial.print(amps); 
-  Serial.println("");
-  
+  double peakVoltage = (5.0 / 1024) * amplitude / 2;
+  double rms = peakVoltage / 1.41421356237;
+  double amps = rms * 30; // TODO: what is 30?
+  if (message == NULL) {
+    Serial.println("message malloc failed.");
+  } else {
+    char* buf;
+    if ((buf = (char *)malloc(20)) == NULL) {
+      Serial.println("buf malloc failed");
+    } else {
+      if (sprintf(message, "%d\t%s\t%f\t%f", amplitude, ftoa(buf, peakVoltage, 1), rms, amps) <= 0) {
+        message = "sprintf failed.";
+      }
+      Serial.println(message);
+      free(buf);
+    }
+  }
   delay(5000);          
 }
 
