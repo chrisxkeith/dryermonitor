363f4023cdd62ac040773d7df19274721ec03691
chrisxkeith <chris.keith@gmail.com>
2015-06-24 06:30:17 -0700

    Add code (not tested) to detect when dryer is drying.

--- readCurrentSensor/readCurrentSensor.ino
+++ readCurrentSensor/readCurrentSensor.ino
@@ -1,4 +1,11 @@
+// http://opensource.org/licenses/MIT
+// Please credit : chris.keith@gmail.com
+// ... IN PROGRESS ...
+
 #include <stdio.h>
+
+const long MAX_LONG = 2147483647;
+const unsigned long MAX_UNSIGNED_LONG = 4294967295;
 
 const int sensorPin = A0;	// select the input pin for the electrical current sensor.
 char* message;
@@ -8,6 +15,10 @@ void setup() {
   if ((message = (char *)malloc(255)) == NULL) {
     Serial.println("message malloc failed.");
   }
+}
+
+unsigned long minSecToMillis(unsigned long minutes, unsigned long seconds) {
+  return (minutes * 60 * 1000) + (seconds * 1000);
 }
 
 void toTwitter(char* message) {
@@ -33,7 +44,7 @@ char * allocAndFormat(char** bufPtr, double d) {
     return ftoa(*bufPtr, d, 1);
 }
 
-void doLoop() {
+double readAmps() {
   int minVal = 1024;
   int maxVal = 0;
   const int iters = 150;
@@ -41,13 +52,15 @@ void doLoop() {
     int sensorValue = analogRead(sensorPin); 
     minVal  = min(sensorValue, minVal);
     maxVal  = max(sensorValue, maxVal);
-    delay(1);
+    delay(1L);
   }
+  
   int amplitude = maxVal - minVal;
   double peakVoltage = (5.0 / 1024) * amplitude / 2;
   double rms = peakVoltage / 1.41421356237;
   double amps = rms * 30; // TODO: what is 30?
   double watts = peakVoltage * amps;
+  
   char* voltageBuf;
   allocAndFormat(&voltageBuf, peakVoltage);
   char* rmsBuf;
@@ -65,12 +78,32 @@ void doLoop() {
   free(rmsBuf);
   free(ampsBuf);
   free(wattsBuf);
+  
+  return amps;
+}
+
+void monitorDryer() {
+  const double DRYER_AMPS = 7.0;
+  const double DRYER_THRESHOLD = DRYER_AMPS * 0.9;
+  const unsigned long WRINKLE_GUARD_SLEEP = minSecToMillis(4, 45);
+  const unsigned long WRINKLE_GUARD_ACTIVE = minSecToMillis(0, 15);
+
+  while (readAmps() < DRYER_THRESHOLD) {
+    delay(60 * 1000);    
+  }
+  // Could be in a drying cycle or in a wrinkle guard active state.
+  unsigned long t = millis();
+  const unsigned long MAX_WRINKLE_GUARD_CYCLES = 50; // if we ever hit this, there's a bug.
 }
 
 void loop() {
   if (message != NULL) {
-    doLoop();
+    readAmps();
+    // Until code has been tested and debugged.
+    // monitorDryer();
+    delay(minSecToMillis(60, 0));          
+  } else {
+    delay(MAX_UNSIGNED_LONG);
   }
-  delay(3000);          
 }
 
